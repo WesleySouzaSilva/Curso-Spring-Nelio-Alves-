5a68f957f980073871e0cb44a1947e7bec140945
Wesley Souza <wesley_souza19982010@hotmail.com>
2021-05-10 12:39:47 -0300

    Total e SubTotal do pedido

--- src/main/java/com/wsystec/cursomc/domain/Pedido.java
+++ src/main/java/com/wsystec/cursomc/domain/Pedido.java
@@ -29,11 +29,9 @@ public class Pedido implements Serializable {
 	@JsonFormat(pattern = "dd/MM/yyyy hh:mm")
 	private Date instante;
 
-	
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento;
 
-	
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
@@ -64,6 +62,15 @@ public class Pedido implements Serializable {
 		this.instante = instante;
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
+	}
+
+	public double getValorTotal() {
+		double soma = 0;
+		for (ItemPedido ip : itens) {
+			soma = soma + ip.getSubTotal();
+		}
+
+		return soma;
 	}
 
 	public Integer getId() {
